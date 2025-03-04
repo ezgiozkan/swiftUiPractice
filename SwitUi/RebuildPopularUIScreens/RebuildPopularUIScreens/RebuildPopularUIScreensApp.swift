80ae9c1f1c76972357a526d58ca05b7ab8d37671
Ezgi Özkan <ezgi.ozkan@ekos.ai>
2025-03-04 11:52:54 +0300

    Initialized new SwiftUI project: RebuildPopularUIScreens

---
+++ SwitUi/RebuildPopularUIScreens/RebuildPopularUIScreens/RebuildPopularUIScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebuildPopularUIScreensApp.swift
+//  RebuildPopularUIScreens
+//
+//  Created by Ezgi Özkan on 4.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RebuildPopularUIScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
