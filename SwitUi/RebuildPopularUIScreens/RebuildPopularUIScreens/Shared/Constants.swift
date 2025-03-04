e1018d8b95bec682b7be4c134a4d12903c16e599
Ezgi Özkan <ezgi.ozkan@ekos.ai>
2025-03-04 11:57:11 +0300

    Added shared utility files for data handling and constants
    	•	Constants.swift: Added global constants for app-wide usage.
    	•	DatabaseHelper.swift: Created a helper class for asynchronously fetching User and Product data from the API.
    	•	Product.swift: Defined the Product model conforming to Codable.
    	•	User.swift: Defined the User model conforming to Codable.

---
+++ SwitUi/RebuildPopularUIScreens/RebuildPopularUIScreens/Shared/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RebuildPopularUIScreens
+//
+//  Created by Ezgi Özkan on 4.03.2025.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
---
+++ SwitUi/RebuildPopularUIScreens/RebuildPopularUIScreens/Shared/Product.swift
@@ -0,0 +1,27 @@
+//
+//  Product.swift
+//  RebuildPopularUIScreens
+//
+//  Created by Ezgi Özkan on 4.03.2025.
+//
+
+import Foundation
+
+struct ProductArray: Codable {
+    let products: [Product]
+    let total, skip, limit: Int
+}
+
+struct Product: Codable, Identifiable {
+    let id: Int
+    let title, description: String
+    let price, discountPercentage, rating: Double
+    let stock: Int
+    let tags: [String]
+    let brand: String?
+    let sku: String
+    let weight: Int
+    let thumbnail: String
+    let images: [String]
+
+}
