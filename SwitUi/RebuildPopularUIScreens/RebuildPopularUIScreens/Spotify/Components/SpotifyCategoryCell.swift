fabbba77d00d8548cd3bc5a61304628ea50dcda7
Ezgi Özkan <ezgi.ozkan@ekos.ai>
2025-03-04 12:29:21 +0300

    Implemented HomeView.swift in the Spotify module to structure the main UI.
    Added SpotifyCategoryCell.swift for displaying category-based content

---
+++ SwitUi/RebuildPopularUIScreens/RebuildPopularUIScreens/Spotify/Components/SpotifyCategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyCategoryCell.swift
+//  RebuildPopularUIScreens
+//
+//  Created by Ezgi Özkan on 4.03.2025.
+//
+
+import SwiftUI
+
+struct SpotifyCategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyCategoryCell()
+}
